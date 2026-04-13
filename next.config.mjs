7db563daa8710ae0f0c30b73ecdaa9378799c04a
Lance Hsu <lance@klh.app>
2026-04-13 19:49:55 +0800

    Remove unused /stats/api/send rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -5,18 +5,8 @@ const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
-      {
-        source: '/stats/stats.js',
-        destination: 'https://analytics.klh.app/script.js',
-      },
-      {
-        source: '/stats/api/send',
-        destination: 'https://analytics.klh.app/api/send',
-      },
-      {
-        source: '/stats/event',
-        destination: 'https://analytics.klh.app/event',
-      },
+      { source: '/stats/stats.js', destination: 'https://analytics.klh.app/script.js' },
+      { source: '/stats/event', destination: 'https://analytics.klh.app/event' },
     ];
   },
 };
